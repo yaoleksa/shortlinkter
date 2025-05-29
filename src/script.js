f52e3904e7c7e25d70c8bdda29bc3ca5f3b6ffe5
yaoleksa <mryaremchyk@gmail.com>
2025-05-29 14:37:00 +0300

    have improved UI logic

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
 document.getElementById('button').addEventListener('click', () => {
     const APIcall = new XMLHttpRequest();
     APIcall.open('POST', 'http://127.0.0.1:5000');
-    APIcall.send({
+    APIcall.send(JSON.stringify({
         "link": document.getElementById('input').value
-    });
+    }));
     console.log(APIcall);
 });
