fe02fda9bec58632b4c7a7500de646c091786bcc
yaoleksa <mryaremchyk@gmail.com>
2025-06-03 08:25:43 +0300

    have added clear input feature

--- src/script.js
+++ src/script.js
@@ -1,15 +1,21 @@
-document.getElementById('button').addEventListener('click', () => {
+const buttons = document.getElementsByClassName('button');
+const urlInput = document.getElementById('input');
+
+buttons[0].addEventListener('click', () => {
     const APIcall = new XMLHttpRequest();
     const result = document.getElementById('result');
     const resultLabel = document.getElementById('result-label');
     APIcall.open('POST', 'http://127.0.0.1:5000');
     APIcall.onloadend = () => {
-        console.log();
         resultLabel.innerText = "Cut down link: ";
         result.setAttribute('href', APIcall.responseText);
         result.innerText = APIcall.responseText;
     }
     APIcall.send(JSON.stringify({
-        "link": document.getElementById('input').value
+        "link": urlInput.value
     }));
+});
+
+buttons[1].addEventListener('click', () => {
+    urlInput.value = null;
 });
