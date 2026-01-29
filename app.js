91d386047f81cecd3f98911be0dea88635d7c584
yaoleksa <mryaremchyk@gmail.com>
2026-01-29 13:37:12 +0200

    trying to fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const idGen = new ShortUniqueId({ length: idLength });
 const HOST = process.env.HOSTNAME || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
+console.log(http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     if(req.method === 'POST') {
         const body = [];
@@ -59,6 +59,4 @@ http.createServer((req, res) => {
         });
         res.end();
     }
-}).listen(PORT, HOST, () => {
-    console.log(`http://${HOST}:${PORT}`);
-});
+}).listen().address());
