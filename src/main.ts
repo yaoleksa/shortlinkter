62d60e64eaadbf97ccf3008f43da73e78b1c1854
yaoleksa <mryaremchyk@gmail.com>
2026-03-12 15:08:05 +0200

    continue fixing Heroku issues

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT ?? 3000;
-  const HOST = process.env.HOST ?? 'localhost';
+  const HOST = process.env.HOST ?? '0.0.0.0';
   await app.listen(PORT, HOST, () => {
     console.log(`http://${HOST}:${PORT}`);
   });
