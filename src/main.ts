bb235300de1a6b82dd613b745639d19b9e050874
yaoleksa <mryaremchyk@gmail.com>
2026-05-15 15:49:07 +0300

    Have tried to fix the CORS problem

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['yaoleksa.github.io'],
+    origin: ['https://yaoleksa.github.io'],
     methods: ['GET', 'POST']
   })
   const PORT = process.env.PORT ?? 3000;
